8763dba412af8d3446711bf2f5e998a24c21df03
artem <artemxgruden@gmail.com>
2018-03-18 10:58:29 +0300

    mod connect & updater

--- functions/Updater.mjs
+++ functions/Updater.mjs
@@ -17,9 +17,108 @@ export default class Updater extends Component {
         o[ subscribeToField ] = nextProps.dataState[ subscribeToField ]
       }
     }
+    // debugger
     return o
   }
+
+  // componentWillUpdate( d1, d2, d3 ){
+  //   debugger
+  // }
+  // shouldComponentUpdate(nextProps, nextState){
+  //   // this.state
+  //   // debugger
+  //   // return true
+  //   let result = false
+  //   debugger
+  //   for ( const subscribeToField of nextProps.subscribeTo ){
+  //     // debugger
+  //     if ( JSON.stringify( this.state[ subscribeToField ] ) !== JSON.stringify( nextProps.dataState[ subscribeToField ] ) ){
+  //       return true
+  //     }
+  //   }
+
+  //   return result
+
+
+  //   // for ( const oldp of nextProps.dataState[ subscribeToField ] )
+
+
+
+  // }
+
+  // static getDerivedStateFromProps( nextProps, prevState ){
+  //   debugger
+  //   return {}
+  // }
+
+
+
+
+
+  shouldComponentUpdate(nextProps, nextState){
+
+    // if ( nextProps.subscribeTo.find( e => e === 'constructions2' ) ){
+      // this.state
+      // nextProps
+      // nextState
+      
+      // const newstateo = {}
+
+      // for ( const subscribeToField of nextProps.subscribeTo ){
+      //   newstateo[ subscribeToField ] = nextProps.dataState[ subscribeToField ]
+      //   // if ( this.state[ subscribeToField ] !== nextProps.dataState[ subscribeToField ] ){
+      //   //   o[ subscribeToField ] = nextProps.dataState[ subscribeToField ]
+      //   // }
+      // }
+      // debugger
+
+      // if ( JSON.stringify( this.state ) === JSON.stringify( newstateo ) && this.props.containerChildren === nextProps.containerChildren ){
+      if ( JSON.stringify( this.state ) === JSON.stringify( nextState ) && this.props.containerChildren === nextProps.containerChildren ){
+
+        // debugger
+        return false
+      
+      } else {
+        // debugger
+        return true
+      
+      }
+    // }
+    // return true 
+  }
+
+    
+  //   this.state
+    
+  //   // if ( JSON.stringify( this.state ) === JSON.stringify( nextState ) ){
+  //   //   debugger
+  //   //   return false
+  //   // } else {
+  //   //   debugger
+  //   //   return true
+  //   // }
+  //   const newstateo = {}
+
+  //   for ( const subscribeToField of nextProps.subscribeTo ){
+  //     newstateo[ subscribeToField ] = nextProps.dataState[ subscribeToField ]
+  //     // if ( this.state[ subscribeToField ] !== nextProps.dataState[ subscribeToField ] ){
+  //     //   o[ subscribeToField ] = nextProps.dataState[ subscribeToField ]
+  //     // }
+  //   }
+  //   debugger
+
+  //    if ( JSON.stringify( this.state ) === JSON.stringify( newstateo ) ){
+  //     // debugger
+  //     return false
+  //   } else {
+  //     // debugger
+  //     return true
+  //   }
+
+    
+  // }
   render(){
+    // debugger
     return createElement( this.props.children, { ...this.state, set: this.props.set, defaultDataStateKeyValues: this.props.defaultDataStateKeyValues, ...this.props.containerProps }, this.props.containerChildren )
   }
 }
--- functions/connect.mjs
+++ functions/connect.mjs
@@ -6,14 +6,24 @@ import { dataStateContext, defaultDataStateKeyValuesContext, setContext } from '
 import Updater from './Updater.mjs'
 
 export default dataStateKeyNames => Container => {
-  
+  // debugger
   class Linker extends Component {
     
     constructor( props ){
       super( props )
+      this.state = {}
+    }
+    // static getDerivedStateFromProps( nextProps, prevState ){
+    //   debugger
+    //   return {}
+    // }
+    shouldComponentUpdate(nextProps, nextState){
+      // debugger
+      return false
     }
 
     render(){
+      // debugger
       return createElement( dataStateContext.Consumer, {},
         dataState => createElement( defaultDataStateKeyValuesContext.Consumer, {},
           defaultDataStateKeyValues => createElement( setContext.Consumer, {},
